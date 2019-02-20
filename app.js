ba4811f24f0ff3e751ac9e140e9e711e60c132ff
Clément Fassot <kedketh@gmail.com>
2019-02-20 08:54:22 +0100

    Add webpack

--- app.js
+++ app.js
@@ -2,6 +2,18 @@ import Player from "./classes/player.js";
 import Monster from "./classes/monster.js";
 import Battle from "./classes/battle.js";
 import Item from "./classes/item.js";
+// import firebase from "./node_modules/firebase";
+
+// const db = firebase.initializeApp({
+//   apiKey: "AIzaSyC6BCgjCW2PRDiMIdV9HhTULmkgbMxVs6U",
+//   authDomain: "saom-88cd9.firebaseapp.com",
+//   databaseURL: "https://saom-88cd9.firebaseio.com",
+//   projectId: "saom-88cd9",
+//   storageBucket: "saom-88cd9.appspot.com",
+//   messagingSenderId: "594008816413"
+// });
+
+// console.log(db.firestore());
 
 const items = {
   sword: new Item("Epée", 0, 10, 1),
