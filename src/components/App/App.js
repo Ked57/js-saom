d905fffce32375ecb4c2981bcc7228f3669bc80f
Clément Fassot <kedketh@gmail.com>
2019-03-01 14:43:52 +0100

    Css improvements

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,18 +2,29 @@ import React, { Component } from "react";
 import "./App.css";
 import ActorStats from "../ActorStats/ActorStats";
 import Grid from "@material-ui/core/Grid";
+import Player from "../../classes/player";
+import Monster from "../../classes/monster";
 
 class App extends Component {
+  splitActors(actors) {
+    console.log(actors);
+    return [
+      actors.filter(actor => actor instanceof Player),
+      actors.filter(actor => actor instanceof Monster)
+    ];
+  }
+
   render() {
+    const [players, monsters] = this.splitActors(this.props.actors);
     return (
       <div className="App">
-        <Grid
-          container
-          direction="column"
-          justify="flex-start"
-          alignItems="center"
-        >
-          {this.props.actors.map(obj => (
+        <Grid container direction="row" justify="center" alignItems="center">
+          {players.map(obj => (
+            <ActorStats key={obj.name} actor={obj} />
+          ))}
+        </Grid>
+        <Grid container direction="row" justify="center" alignItems="center">
+          {monsters.map(obj => (
             <ActorStats key={obj.name} actor={obj} />
           ))}
         </Grid>
--- src/index.js
+++ src/index.js
@@ -24,33 +24,26 @@ const baseMonsterStats = {
 };
 
 const initPage = querySnapshot => {
-  if (querySnapshot.size > 1) {
-    console.error("CA MARCHE PAS Y'A TROP DE GENS");
-    return;
-  }
-
-  const playerDoc = querySnapshot.docs.pop();
-  const playerData = playerDoc.data();
-
-  const items = Object.keys(playerData.items).map(
-    key =>
-      new Item(
-        playerData.items[key].name,
-        playerData.items[key].healthPoints,
-        playerData.items[key].attackPoints,
-        playerData.items[key].defensePoints
-      )
-  );
-  const player = new Player(playerData.name, "photo", playerData.stats, items);
+  const players = querySnapshot.docs.map(playerDoc => {
+    const playerData = playerDoc.data();
+    const items = Object.keys(playerData.items).map(
+      key =>
+        new Item(
+          playerData.items[key].name,
+          playerData.items[key].healthPoints,
+          playerData.items[key].attackPoints,
+          playerData.items[key].defensePoints
+        )
+    );
+    return new Player(playerData.name, "photo", playerData.stats, items);
+  });
+  console.log(players);
   const david = new Monster("David", "image", baseMonsterStats, {});
-  const actors = [player, david];
+  const actors = [...players, david];
   ReactDOM.render(<App actors={actors} />, document.getElementById("root"));
 };
 
-playerCollection
-  .where("name", "==", "player")
-  .get()
-  .then(initPage);
+playerCollection.get().then(initPage);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
